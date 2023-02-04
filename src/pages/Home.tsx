0c8db05522951391d3e05cf80c57859a99e4edca
TrishtenAcharya <acharyaP03@gmail.com>
2023-02-05 01:07:32 +1100

    initial home page layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import millify from "millify";
+import { Typography, Row, Col, Statistic, Spin } from "antd";
+import {useGetCryptosQuery} from "../services/cryptoApi";
+
+const { Title }  = Typography;
 
 
 const Home = (): JSX.Element =>{
+    // @ts-ignore
+    const { data, isFetching } = useGetCryptosQuery();
+    const globalStats = data?.data.stats
+
+    if(isFetching) return <Spin />;
+
+
+
     return(
-        <h1>Home page </h1>
+        <>
+            <Title level={2} className="heading">Global Crypto Stats</Title>
+            <Row>
+                <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total}/></Col>
+                <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)}/></Col>
+                <Col span={12}><Statistic title="Total Market Caps" value={millify(globalStats.totalMarketCap)}/></Col>
+                <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)}/></Col>
+                <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)}/></Col>
+            </Row>
+        </>
     )
 }
 
