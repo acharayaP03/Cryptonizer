4fece5c587d4847926f59437e15513820b4aa279
TrishtenAcharya <acharyaP03@gmail.com>
2023-02-05 01:07:56 +1100

    redux toolkit setup for cryptoApi

---
+++ src/services/cryptoApi.ts
@@ -0,0 +1,23 @@
+
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Key': '2406d572c4msh3b45e698894d40fp17782ejsn51cb7954b272',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+const createRequest = (url: string) => ({ url, headers: cryptoApiHeaders})
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl}),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({ query: () => createRequest('/coins')})
+    })
+
+})
+
+export const {
+    useGetCryptosQuery
+} = cryptoApi
