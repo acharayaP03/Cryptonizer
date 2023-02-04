13b1dfa8b89d32172c8f4e77fe9b32bcc37b9f08
TrishtenAcharya <acharyaP03@gmail.com>
2023-02-05 01:13:27 +1100

    add components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import millify from "millify";
 import { Typography, Row, Col, Statistic, Spin } from "antd";
 import {useGetCryptosQuery} from "../services/cryptoApi";
+import {Cryptocurrencies} from "./index";
+import News from "./News";
 
 const { Title }  = Typography;
 
@@ -27,6 +29,20 @@ const Home = (): JSX.Element =>{
                 <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)}/></Col>
                 <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)}/></Col>
             </Row>
+            <div className="home-heading-container">
+                <Title level={2} className="home-title">Top 10 Cryptocurrencies in the world</Title>
+                <Title level={2} className="home-title">
+                    <Link to='/'>Show more</Link>
+                </Title>
+            </div>
+            <Cryptocurrencies/>
+            <div className="home-heading-container">
+                <Title level={2} className="home-title">Latest Crypto News</Title>
+                <Title level={2} className="home-title">
+                    <Link to='/'>Show more</Link>
+                </Title>
+            </div>
+            <News/>
         </>
     )
 }
