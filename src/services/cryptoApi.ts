42a6b004370bb07e63e230b0d2352c323f00b996
TrishtenAcharya <acharyaP03@gmail.com>
2023-02-06 00:35:02 +1100

    there is a bug in this page fix it

--- src/services/cryptoApi.ts
+++ src/services/cryptoApi.ts
@@ -7,11 +7,15 @@ export const cryptoApi = createApi({
     reducerPath: 'cryptoApi',
     baseQuery: fetchBaseQuery({ baseUrl}),
     endpoints: (builder) => ({
-        getCryptos: builder.query({ query: (count) => createRequest(`/coins?limit=${count}`)})
+        getCryptos: builder.query({ query: (count) => createRequest(`/coins?limit=${count}`)}),
+        getCryptoDetails: builder.query({ query: (coinId) => createRequest(`/coin/${coinId}`)}),
+        getCryptoHistory: builder.query({ query: ({coinId, timeperiod}) => createRequest(`/coin/${coinId}/history/${timeperiod}`)})
     })
 
 })
 
 export const {
-    useGetCryptosQuery
+    useGetCryptosQuery,
+    useGetCryptoDetailsQuery,
+    useGetCryptoHistoryQuery
 } = cryptoApi
