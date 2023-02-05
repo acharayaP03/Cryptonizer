25a10a9ffe77470cd79bec8eb9d35dfca01f767b
TrishtenAcharya <acharyaP03@gmail.com>
2023-02-05 22:20:21 +1100

    select option

--- src/pages/News.tsx
+++ src/pages/News.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Select, Typography, Row, Col, Avatar, Card, Spin} from "antd";
 
 import moment from 'moment';
 import {useGetCryptoNewsQuery} from "../services/createCryptoNewsApi";
+import {useGetCryptosQuery} from "../services/cryptoApi";
 
 const { Text, Title } = Typography;
 const { Option } = Select;
@@ -12,16 +13,37 @@ interface Simplified {
 }
 
 const News: React.FC<Simplified> = ({ simplified }) =>{
+    const [newsCategory, setNewsCategory] = useState('Cryptocurrency')
 
-    const { data: cryptoNews, isFetching, error } = useGetCryptoNewsQuery({ newsCategory: 'crypto currency', count: simplified ? 6 : 12 });
+    const { data: cryptoNews, isFetching, error } = useGetCryptoNewsQuery({ newsCategory, count: simplified ? 6 : 12 });
+    const { data } = useGetCryptosQuery(100)
+
 
     const demoImage = 'https//coinrevolution.com/wp-content/uploads/2020/06/cryptonews.jpg';
 
     if(isFetching) return <Spin className="loader"/>
-    if(error) return <Spin className="loader"/>
+    if(error) return <p>Opps error occured</p>
 
+    // @ts-ignore
     return(
         <Row gutter={[24, 24]}>
+            { !simplified && (
+                <Col span={24}>
+                    <Select
+                        showSearch
+                        className="select-news"
+                        placeholder="Select Crypto"
+                        optionFilterProp="children"
+                        onChange={(value) => setNewsCategory(value)}
+                        // @ts-ignore
+                        filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+                    >
+                        <Option value="Cryptocurrency">Cryptocurrency</Option>
+                        { data?.data?.coins.map(({ name }: { name: string}) => <Option value={name}>{name}</Option>)}
+                    </Select>
+                </Col>
+            )}
+
             {
                 // @ts-ignore
                 cryptoNews.value.map((news, i) => (
